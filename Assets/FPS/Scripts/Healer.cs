eb42c96e407d3e949bcdd534bf5be89801b8930c
Toby Wisener <toby@wisener.me>
2021-02-16 20:17:25 +0000

    Initial commit of Unity project

---
+++ Assets/FPS/Scripts/Healer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healer : MonoBehaviour
+{
+    private float update;
+
+    public GameObject[] HealNPCs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        update = 0.0f;
+        if (HealNPCs.Length == 0)
+        {
+            return;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject healingNpc = HealNPCs[0];
+        if (healingNpc == null)
+        {
+            // Don't process for invalid npc
+            return;
+        }
+
+        update += Time.deltaTime;
+        if (update > 3.0f /* Every 3 seconds */)
+        {
+            Health npcHealth = healingNpc.GetComponent<Health>();
+            Debug.Log("Health is currenly: " + npcHealth.currentHealth);
+            npcHealth.Heal(Random.Range(1, 50));
+            update = 0.0f; // Reset the interval
+        }
+        
+    }
+}
